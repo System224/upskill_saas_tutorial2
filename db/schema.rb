daf7e1cdcdff7b175e36d0890119cff200ed54cc
Wilfredo Chauran <wilfredochauran24@gmail.com>
2020-08-26 19:17:29 +0000

    Added Stripe customer token to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200823223255) do
+ActiveRecord::Schema.define(version: 20200826191508) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
